6cf525c58660e0bb6e887a558bf50caab7497e74
AyanSayyad07 <ayan.sayyad.04.2004@gmail.com>
2026-05-10 10:18:38 +0000

    Fix

---
+++ rewrite_history.sh
@@ -0,0 +1,15 @@
+git filter-branch -f --env-filter '
+OLD_EMAIL="hackathon@example.com"
+CORRECT_NAME="AyanSayyad07"
+CORRECT_EMAIL="ayan.sayyad.04.2004@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
